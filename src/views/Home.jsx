d8db10e14e1547bb243203fe6973ee9682ead743
azmiruddin@outlook.com <azmiruddin@outlook.com>
2020-02-28 17:58:37 +0100

    AWT PWA - Media Playback Initial commit

---
+++ src/views/Home.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Container, Row, Col,CardGroup, Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle,Button} from 'reactstrap';
+import { MainHeadline } from '../components/Headline/MainHeadline';
+import { SecondaryHeadline } from '../components/Headline/SecondaryHeadline';
+import videoSources from '../videoSources';
+import VideoItem from '../components/VideoFeed/VideoItem';
+import ViewLayout from '../components/ViewLayout';
+import { Offline, Online } from 'react-detect-offline';
+
+const HeadlineWrapper = styled.div`
+  text-align: center;
+  color: #ffffff;
+`;
+
+const HomeView = () => (
+  <ViewLayout>
+
+  
+
+    <Online polling={{ enabled: true, interval: 500 }}>
+      <Container>
+        <HeadlineWrapper>
+          <MainHeadline>
+            My Playlist
+          </MainHeadline>
+          {/* <SecondaryHeadline>It’s quick, simple and eﬃcient.</SecondaryHeadline> */}
+        </HeadlineWrapper>
+        
+       <CardGroup>
+         <table bordered style={{marginLeft:"12%"}}>
+           <tbody>
+            {videoSources.map((video, i) => <VideoItem {...video} key={i} />)}
+            </tbody>
+          </table>
+      </CardGroup>
+      </Container>
+    </Online>
+    <Offline polling={{ enabled: true, interval: 500 }}>
+      <Container>
+        <MainHeadline>The application is currently used offline.</MainHeadline>
+        <SecondaryHeadline>
+          You can only watch your downloaded videos under{' '}
+          <a className="button-link" href="/downloads">
+            MY DOWNLOADS
+          </a>.
+        </SecondaryHeadline>
+      </Container>
+    </Offline>
+  </ViewLayout>
+);
+
+export default HomeView;
